40df94a75a85d0d81af527a265daa4e28c84a49e
Eshika <eshikagupta159@gmail.com>
2019-04-17 15:11:10 +0530

    Add files via upload

--- public/javascripts/ResetScript.js
+++ public/javascripts/ResetScript.js
@@ -1,49 +1,24 @@
-var req;
-	
-function checkajax(){
-    try{
-    req = new XMLHttpRequest();
-    console.log("///////////////////ajax////");
-    /* alert("xmlhttprequest done") */
-    return;
-    }
-    catch(e){}
-    
-    try{
-        req = new ActiveXObject("Msxm2.XMLHTTP");
-        /* alert("activex msxm2 done"); */
-        return;
-    }
-    catch(e){}
-    
-    try{
-        req = new ActiveXObject("Microsoft.XMLHTTP");
-        /* alert("activex microsoft xmlhttp done"); */
-        return;
-    }catch(e){}
-    console.log("///////////////////ajax////");
-    alert("your browser doesnot support ajax");
-}
 
-function checkPassword(name){
-    var newPass=document.forms[name]['newPass'].value;
-    var confirmPass=document.forms[name]['confirmPass'].value;
-    
-    var Passregex=new RegExp(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{4,}$/);
-    var passmsg='';
-    var newpassmsg='';
-    if(!Passregex.test(newPass)){
+//PASSWORD VALIDATION IN FORGOT PASSWORD
+function checkPassword(name) {
+    var newPass = document.forms[name]['newPass'].value;
+    var confirmPass = document.forms[name]['confirmPass'].value;
+
+    var Passregex = new RegExp(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{4,}$/);
+    var passmsg = '';
+    var newpassmsg = '';
+    if (!Passregex.test(newPass)) {
         console.log(Passregex.test(newPass));
-        newpassmsg='Your password should contain atleast 1 uppercase,1number and min 4 characters';
+        newpassmsg = 'Your password should contain atleast 1 uppercase,1number and min 4 characters';
     }
 
-    if(newPass!=confirmPass||confirmPass.length==0){
-        passmsg='Your New Password and Confirm password doesnot match';
- }
+    if (newPass != confirmPass || confirmPass.length == 0) {
+        passmsg = 'Your New Password and Confirm password doesnot match';
+    }
 
-    if(passmsg!=''||newpassmsg!=''){
-        document.getElementById('newPass').innerHTML=newpassmsg;
-        document.getElementById('confirmPass').innerHTML=passmsg;
+    if (passmsg != '' || newpassmsg != '') {
+        document.getElementById('newPass').innerHTML = newpassmsg;
+        document.getElementById('confirmPass').innerHTML = passmsg;
         return false;
     }
     return true;
---
+++ public/javascripts/machineScript.js
@@ -0,0 +1,171 @@
+
+var req;
+
+function checkajax() {
+    try {
+        req = new XMLHttpRequest();
+        console.log("///////////////////ajax////");
+        /* alert("xmlhttprequest done") */
+        return;
+    }
+    catch (e) { }
+
+    try {
+        req = new ActiveXObject("Msxm2.XMLHTTP");
+        /* alert("activex msxm2 done"); */
+        return;
+    }
+    catch (e) { }
+
+    try {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+        /* alert("activex microsoft xmlhttp done"); */
+        return;
+    } catch (e) { }
+    console.log("///////////////////ajax////");
+    alert("your browser doesnot support ajax");
+}
+
+
+//POPULATING DATA IN EDIT MACHINE MODAL
+function edit(id) {
+    console.log("------------------>Inside edit" + id);
+    var url = "/machine/getmachine/" + id;
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            var obj = JSON.parse(req.responseText);
+            console.log(req.responseText);
+
+            document.getElementById("machineID").value = obj.machineID;
+            document.getElementById("machineName").value = obj.machineName;
+            document.getElementById("serialNo").value = obj.serialNo;
+            document.getElementById("modelID").value = obj.modelID;
+            document.getElementById("ip").value = obj.ip;
+            document.getElementById("machineStatus").value = obj.machineStatus;
+            document.getElementById("reset").value = obj.machineID;
+
+        }
+    }
+
+    req.open("GET", url, true);
+    req.send();
+
+}
+
+
+//VALIDATING MACHINE MODAL
+function checkValidate(name) {
+    console.log("---------->check validate");
+
+    var machineID = document.forms[name]['machineID'].value;
+    var machineName = document.forms[name]['machineName'].value;
+    var serialNo = document.forms[name]['serialNo'].value;
+    var modelID = document.forms[name]["modelID"].value;
+    var ip = document.forms[name]['ip'].value;
+
+    var machineReg = new RegExp('^([a-zA-Z]{2}[0-9]{2,})$');
+    var serialReg = new RegExp('^([0-9]{4})$');
+    var ipReg = new RegExp('^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$');
+
+    var machineIDvalid = '';
+    var machineNameValid = '';
+    var serailNoValid = '';
+    var modelIDValid = '';
+    var ipValid = '';
+
+    if (machineID.length == 0 || machineID.length < 4 || !machineReg.test(machineID)) {
+        console.log("------------->" + machineReg.test(machineID.value));
+        machineIDvalid = "Please enter ID with first 2 letters and next digit";
+    }
+
+    if (machineName.length < 4 || machineName.length > 15) {
+        machineNameValid = "Please enter Name with min 4 and max 15 characters";
+    }
+    if (serialNo.length != 4 || !serialReg.test(serialNo)) {
+        console.log("Serial no " + serialReg.test(serialNo));
+        serailNoValid = "Please enter digit[0-9] of length 4";
+    }
+    if (modelID.length == 0 || modelID.length < 4 || !machineReg.test(modelID)) {
+        console.log("------------->ModelID " + machineReg.test(modelID.value));
+        modelIDValid = "Please enter ID with first 2 letters and next 2 digit";
+    }
+    if (!ipReg.test(ip)) {
+        ipValid = "Please enter valid IP Adrress";
+    }
+
+    if (machineIDvalid != '' || machineNameValid != '' || serailNoValid != '' || modelIDValid != '' || ipValid != '') {
+        document.getElementById(name + 'machineID').innerHTML = machineIDvalid;
+        document.getElementById(name + 'machineName').innerHTML = machineNameValid;
+        document.getElementById(name + 'serialNo').innerHTML = serailNoValid;
+        document.getElementById(name + 'modelID').innerHTML = modelIDValid;
+        document.getElementById(name + 'ip').innerHTML = ipValid;
+
+        return false;
+    }
+
+
+    return true;
+}
+
+function setID(machineid) {
+    document.getElementById('form3machineID').value = machineid;
+
+}
+
+//DELETE MACHINE 
+function delMachine() {
+    var machineid = document.getElementById('form3machineID').value;
+
+    var url = '/machine/delete/' + machineid;
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            alert.log(req.responseText);
+
+
+            window.location.href = '/machine/';
+            location.reload(true);
+
+        }
+    }
+
+    req.open("GET", url, true);
+    req.send();
+}
+
+
+//RESETTING FORM FIELDS
+function resetHandler(name) {
+
+    var machineID = document.getElementById(name + 'machineID').value;
+    var machineName = document.getElementById(name + 'machineName').value;
+    var serialNo = document.getElementById(name + 'serialNo').value;
+    var modelID = document.getElementById(name + 'modelID').value;
+    var ip = document.getElementById(name + 'ip').value;
+    if (machineID != '' || machineName != '' || serialNo != '' || modelID != '' || ip != '') {
+        document.getElementById(name + 'machineID').innerHTML = '';
+        document.getElementById(name + 'machineName').innerHTML = '';
+        document.getElementById(name + 'serialNo').innerHTML = '';
+        document.getElementById(name + 'modelID').innerHTML = '';
+        document.getElementById(name + 'ip').innerHTML = '';
+
+    }
+
+}
+
+
+$(document).ready(function(){
+
+    $('[name="machineID"]').on('input', function() {
+        var machineID=$(this).val();
+        //alert(machineID);
+        var machineIDvalid='';
+        if (machineID.length == 0 || machineID.length < 4 || !machineReg.test(machineID)) {
+            //alert(machineID);
+            console.log("------------->" + machineReg.test(machineID.value));
+            machineIDvalid = "Please enter ID with first 2 letters and next digit";
+            $('#form1machineID').text(machineIDvalid);
+        }
+        
+
+    });
+});
